d5d9cb379ff4e48e9ecf4071e8a0d3aaf4b64ec5
Zetrik <30878725+ztk37@users.noreply.github.com>
2021-10-29 21:15:47 +0200

    clean Flavor.hs

--- Flavor.hs
+++ Flavor.hs
@@ -1,10 +1,6 @@
-module Flavor
-  ( flavor
-  ) where
+module Flavor ( flavor ) where
 
 -- A dummy file to give the repository a Haskell flavor.
 
 flavor :: IO ()
 flavor = return ()
-
--- Flavorfile.hs ?
