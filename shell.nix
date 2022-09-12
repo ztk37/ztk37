678f50d92afea0cd19291465fbcfd32c7235d704
ztk37 <ztk37.contact@gmail.com>
2022-09-12 22:07:52 +0200

    chore: add banner.gif

--- shell.nix
+++ shell.nix
@@ -1,4 +1,13 @@
 { pkgs ? import <nixpkgs> {}
 }: pkgs.mkShell {
     name = "dev-shell";
+
+    buildInputs = [
+        pkgs.figlet
+        pkgs.lolcat
+    ];
+
+    shellHook = ''
+        echo "My GitHub Profile" | figlet | lolcat --freq 0.3
+    '';
 }
